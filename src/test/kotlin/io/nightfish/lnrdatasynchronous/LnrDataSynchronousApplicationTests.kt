60a64a83724832fe3883740283cd322ec8289f14
NightFish <101402767+dmzz-yyhyy@users.noreply.github.com>
2025-02-15 23:04:59 +0800

    init

---
+++ src/test/kotlin/io/nightfish/lnrdatasynchronous/LnrDataSynchronousApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.nightfish.lnrdatasynchronous
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LnrDataSynchronousApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
